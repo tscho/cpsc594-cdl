3c75f69d9b8de253c491ff4a2265cb721414c667
jackyct1 <jackyct1@ad8adf05-5d06-795c-5429-6d1e366d447b>
2010-10-17 03:07:21 +0000

    Updated the files so it is using MVC style now.

---
+++ trunk/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cpsc594_cdl.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Index Page";
+            return View();
+        }
+
+    }
+}
