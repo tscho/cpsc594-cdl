4aac19c452f8d4e5481da06c355e1326f99132be
jackyct1 <jackyct1@ad8adf05-5d06-795c-5429-6d1e366d447b>
2010-10-25 22:04:43 +0000

    Project imported

---
+++ cpsc594-cdl/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cpsc594_cdl.MvcApplication" Language="C#" %>
---
+++ cpsc594-cdl/Models/MainModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cpsc594_cdl.Models
+{
+    public class IndexModel
+    {
+        [Required]
+        public string ProjectID { get; set; }
+        [Required]
+        public string IsSelectProject { get; set; }
+
+        [Required]
+        public string CM_Array { get; set; }
+    }
+}
