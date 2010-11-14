d8cf396dfe35956c16e8d1885a143095ed609d4d
jackyct1 <jackyct1@ad8adf05-5d06-795c-5429-6d1e366d447b>
2010-11-14 23:07:47 +0000

    1) Changed chart to ReportController 2) Added 2 more charts

--- cpsc594-cdl/Controllers/HomeController.cs
+++ cpsc594-cdl/Controllers/HomeController.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI.DataVisualization.Charting;
-using System.Drawing;
 using cpsc594_cdl.Models;
-using System.IO;
 
 namespace cpsc594_cdl.Controllers
 {
@@ -49,35 +45,6 @@ namespace cpsc594_cdl.Controllers
             ViewData["Metrics"] = metrics;
 
             return View();
-        }
-
-        public FileResult GetChart()
-        {
-            List<int> data  = Models.StaticModel.createStaticData();
-            Chart chart_1 = new Chart();
-            chart_1.Width = 800;
-            chart_1.Height = 400;
-            chart_1.RenderType = RenderType.ImageTag;
-            chart_1.Palette = ChartColorPalette.BrightPastel;
-            Title title = new Title("Coverage History", Docking.Top, new Font("Trebuchet MS", 14, FontStyle.Bold), Color.FromArgb(26, 59, 105));
-            chart_1.Titles.Add(title);
-            chart_1.ChartAreas.Add("Chart Area");
-
-            // create a series
-            var series1 = new Series("Series");
-            series1.ChartType = SeriesChartType.FastLine;
-            chart_1.Series.Add(series1);
-
-            // add points to series 1
-            foreach (int value in data)
-            {
-                series1.Points.AddY(value);
-            }
-
-            MemoryStream imageStream = new MemoryStream();
-            chart_1.SaveImage(imageStream, ChartImageFormat.Png);
-            imageStream.Position = 0;
-            return new FileStreamResult(imageStream, "image/png");
         }
     }
 }
