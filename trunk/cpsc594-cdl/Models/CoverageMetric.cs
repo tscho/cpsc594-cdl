253824a9731922caf632a2963dc8f1b5c11cc948
t.schoendorfer <t.schoendorfer@ad8adf05-5d06-795c-5429-6d1e366d447b>
2011-02-03 07:36:35 +0000

    had to remove and re-add due to svn duplicate

---
+++ trunk/cpsc594-cdl/Models/CoverageMetric.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.DataVisualization.Charting;
+using System.Drawing;
+using System.IO;
+using cpsc594_cdl.Common.Models;
+using cpsc594_cdl.Infrastructure;
+
+namespace cpsc594_cdl.Models
+{
+    public class CoverageMetric : IMetric
+    {
+        public Iteration[] Iterations;
+
+        public string Name { get { return "Code Coverage"; }}
+
+        public String GenerateGraph(string title, IEnumerable<Component> components)
+        {
+            Chart chart = ChartFactory.CreateChart(title);
+
+            Series series;
+            foreach (var iteration in Iterations)
+            {
+                if (iteration.Coverages == null || iteration.Coverages.Count == 0)
+                    continue;
+
+                series = new Series(iteration.StartDate.ToShortDateString());
+                chart.Series.Add(series);
+                foreach(CoverageMetricData coverage in iteration.Coverages)
+                {
+                    series.Points.AddXY(coverage.ComponentID, coverage.GetCoverage());
+                    series.Points.Last().MarkerSize = 10;
+                    series.Points.Last().AxisLabel = coverage.Component.ComponentName;
+                }
+            }
+
+            MemoryStream imageStream = new MemoryStream();
+            chart.SaveImage(imageStream, ChartImageFormat.Png);
+            imageStream.Position = 0;
+
+            string base64_output = System.Convert.ToBase64String(imageStream.ToArray());
+            return base64_output;
+        }
+
+        public string GenerateGraph(string title, Component component)
+        {
+            return GenerateGraph(title, new Component[] { component });
+        }
+    }
+}
---
+++ trunk/cpsc594-cdl/Models/DefectInjectionMetric.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.DataVisualization.Charting;
+using System.Drawing;
+using System.IO;
+using cpsc594_cdl.Common.Models;
+using cpsc594_cdl.Infrastructure;
+
+namespace cpsc594_cdl.Models
+{
+    public class DefectInjectionMetric
+    {
+        public Iteration[] Iterations;
+
+        public DefectInjectionMetric(IEnumerable<Iteration> iterations)
+        {
+            Iterations = iterations.ToArray();
+        }
+
+        public String GenerateGraph(string title, IEnumerable<Component> components)
+        {
+            Chart chart = ChartFactory.CreateChart(title);
+
+            Series series;
+            foreach (var component in components)
+            {
+                foreach (var iteration in Iterations)
+                {
+                    if ((series = chart.Series.FindByName(iteration.StartDate.ToShortDateString())) == null)
+                    {
+                        series = new Series(iteration.StartDate.ToShortDateString());
+                        chart.Series.Add(series);
+                    }
+                    series.Points.AddY(iteration.DefectInjectionRates.Single(x => x.ComponentID == component.ComponentID).GetValue());
+                    series.Points.Last().MarkerSize = 10;
+                    series.Points.Last().AxisLabel = component.ComponentName;
+                }
+            }
+
+            MemoryStream imageStream = new MemoryStream();
+            chart.SaveImage(imageStream, ChartImageFormat.Png);
+            imageStream.Position = 0;
+
+            string base64_output = System.Convert.ToBase64String(imageStream.ToArray());
+            return base64_output;
+        }
+
+        public static String GenerateGraph(IEnumerable<Component> components, string title)
+        {
+            return "";
+        }
+    }
+}
