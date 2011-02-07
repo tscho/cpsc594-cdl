c9860e559b25d024f34fa5d0d69c0592beb99f3b
jackyct1@gmail.com <jackyct1@gmail.com@ad8adf05-5d06-795c-5429-6d1e366d447b>
2011-02-07 13:00:00 +0000

    1) Added Title for each axis 2) Added detail chart for each component in Defect Injection Rate, Defect Repair Rate

--- cpsc594-cdl/Models/DefectInjectionMetric.cs
+++ cpsc594-cdl/Models/DefectInjectionMetric.cs
@@ -20,6 +20,10 @@ namespace cpsc594_cdl.Models
         public override string GenerateOverviewGraph(string title, IEnumerable<Component> components)
         {
             Chart chart = ChartFactory.CreateChart(title);
+            chart.ChartAreas[0].AxisX.TitleFont = new Font("Arial", 12, FontStyle.Bold);
+            chart.ChartAreas[0].AxisX.Title = "Components";
+            chart.ChartAreas[0].AxisY.TitleFont = new Font("Arial", 12, FontStyle.Bold);
+            chart.ChartAreas[0].AxisY.Title = "Total Number of Defects\n(High, Medium, Low Defects)";
 
             IEnumerable<int> componentIds = components.Select(x => x.ComponentID);
             Series series;
@@ -29,13 +33,14 @@ namespace cpsc594_cdl.Models
                     continue;
 
                 series = new Series(iteration.StartDate.ToShortDateString());
-                series = new Series(iteration.StartDate.ToShortDateString());
                 chart.Series.Add(series);
                 foreach(var injectionRate in iteration.DefectInjectionRates.Where(x => componentIds.Contains(x.ComponentID)))
                 {
-                    series.Points.AddXY(injectionRate.ComponentID, injectionRate.GetValue());
+                    int value = (int)(injectionRate.GetValue());
+                    series.Points.AddXY(injectionRate.ComponentID, value);
                     series.Points.Last().MarkerSize = 10;
                     series.Points.Last().AxisLabel = injectionRate.Component.ComponentName;
+                    series.Points.Last().Label = "" + value;
                 }
             }
 
@@ -49,7 +54,64 @@ namespace cpsc594_cdl.Models
 
         public override string GenerateComponentGraph(string title, Component component)
         {
-            return GenerateOverviewGraph(title, new Component[] { component });
+            //return GenerateOverviewGraph(title, new Component[] { component });
+            Chart chart = ChartFactory.CreateChart(title);
+            chart.ChartAreas[0].AxisX.TitleFont = new Font("Arial", 12, FontStyle.Bold);
+            chart.ChartAreas[0].AxisX.Title = "Iterations";
+            chart.ChartAreas[0].AxisY.TitleFont = new Font("Arial", 12, FontStyle.Bold);
+            chart.ChartAreas[0].AxisY.Title = "Number of Defects";
+
+            Series series;
+            double value;
+            // High
+            series = new Series("High");
+            chart.Series.Add(series);
+            foreach (var iteration in Iterations)
+            {
+                foreach (var injectionRate in iteration.DefectInjectionRates.Where(x => (component.ComponentID == x.ComponentID)))
+                {
+                    value = (int)injectionRate.NumberOfHighDefects;
+                    series.Points.AddY(value);
+                    series.Points.Last().MarkerSize = 10;
+                    series.Points.Last().AxisLabel = iteration.StartDate.ToShortDateString();
+                    series.Points.Last().Label = "" + value;
+                }
+            }
+            // Medium
+            series = new Series("Medium");
+            chart.Series.Add(series);
+            foreach (var iteration in Iterations)
+            {
+                foreach (var injectionRate in iteration.DefectInjectionRates.Where(x => (component.ComponentID == x.ComponentID)))
+                {
+                    value = (int)injectionRate.NumberOfMediumDefects;
+                    series.Points.AddY(value);
+                    series.Points.Last().MarkerSize = 10;
+                    series.Points.Last().AxisLabel = iteration.StartDate.ToShortDateString();
+                    series.Points.Last().Label = "" + value;
+                }
+            }
+            // Low
+            series = new Series("Low");
+            chart.Series.Add(series);
+            foreach (var iteration in Iterations)
+            {
+                foreach (var injectionRate in iteration.DefectInjectionRates.Where(x => (component.ComponentID == x.ComponentID)))
+                {
+                    value = (int)injectionRate.NumberOfLowDefects;
+                    series.Points.AddY(value);
+                    series.Points.Last().MarkerSize = 10;
+                    series.Points.Last().AxisLabel = iteration.StartDate.ToShortDateString();
+                    series.Points.Last().Label = "" + value;
+                }
+            }
+
+            MemoryStream imageStream = new MemoryStream();
+            chart.SaveImage(imageStream, ChartImageFormat.Png);
+            imageStream.Position = 0;
+
+            string base64_output = System.Convert.ToBase64String(imageStream.ToArray());
+            return base64_output;
         }
     }
 }
