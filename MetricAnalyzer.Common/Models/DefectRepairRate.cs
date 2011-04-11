cdff745a37d925f85b8c86e0f80f32e2a4b99026
t.schoendorfer <t.schoendorfer@ad8adf05-5d06-795c-5429-6d1e366d447b>
2011-04-11 01:17:19 +0000

    Fixed bug in test effectiveness calculation

---
+++ MetricAnalyzer.Common/Models/DefectRepairRate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetricAnalyzer.Common.Models
+{
+    public partial class DefectRepairRate
+    {
+        public double GetValue()
+        {
+            return this.NumberOfResolvedDefects + this.NumberOfVerifiedDefects;
+        }
+    }
+}
